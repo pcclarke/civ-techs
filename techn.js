228f549cf8e7f76fd9b05b9ab7736474dbe1d372
Patrick Clarke <ekralccp@me.com>
2016-05-04 19:33:14 -0700

    Added required squares and optional circles

--- techn.js
+++ techn.js
@@ -2,12 +2,15 @@ var margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 1000 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom;
     
+var arcBase = 155;
+var arcWidth = 20;
+    
 var arc = d3.svg.arc()
     .innerRadius(function(d) {
-        return 180 + (5 * d.arcRank);
+        return arcBase + (arcWidth * d.arcRank);
     })
     .outerRadius(function(d) {
-        return 185 + (5 * d.arcRank);
+        return 160 + (arcWidth * d.arcRank);
     })
     .startAngle(0)
     .endAngle(function(d) {
@@ -35,37 +38,37 @@ d3.json("technologies.json", function(data) {
     for (var i = 0; i < data.technologies.length; i++) {
         var rekked = []; // copy leads to required technologies
         var opted = []; // copy leads to optional technologies
+        var optedDist = [];
         var arcDist = 0;
         
         for (var j = 0; j < data.technologies.length; j++) {
             if (data.technologies[j].requires) {
                 for (var k = 0; k < data.technologies[j].requires.length; k++) {                    
                     if (data.technologies[j].requires[k] === data.technologies[i].name) {
-                        rekked.push(data.technologies[j].name);
                         arcDist = data.technologies[j].pos - data.technologies[i].pos;
+                        var req = {"name": data.technologies[j].name, "dist": arcDist, "pos": data.technologies[i].pos};
+                        rekked.push(req);
                     }
                 }
             }
             if (data.technologies[j].optional) {
                 for (var k = 0; k < data.technologies[j].optional.length; k++) {
                     if (data.technologies[j].optional[k] === data.technologies[i].name) {
-                        opted.push(data.technologies[j].name);
                         arcDist = data.technologies[j].pos - data.technologies[i].pos;
+                        var opt = {"name": data.technologies[j].name, "dist": arcDist, "pos": data.technologies[i].pos};
+                        opted.push(opt);
                     }
                 }
             }
         }
         
-        if (rekked.length > 0) {
-            data.technologies[i].lreq = rekked;
-        }
-        if (opted.length > 0) {
-            data.technologies[i].lopt = opted;
-        }
+        data.technologies[i].lreq = rekked;
+        data.technologies[i].lopt = opted;
+        
         data.technologies[i].arcDist = ((2 * Math.PI) / data.technologies.length) * arcDist;
         
         // Set arc rank - distance of arc from centre
-        if (data.technologies[i].lreq || data.technologies[i].lopt) {
+        if (data.technologies[i].lreq.length > 0 || data.technologies[i].lopt.length > 0) {
             var ranked = 0;
             for (var j = 0; j < arcDists.length; j++) {
                 if (arcDists[j] < i) {
@@ -78,6 +81,14 @@ d3.json("technologies.json", function(data) {
             if (ranked == 0) {
                 arcDists.push(i + arcDist);
                 data.technologies[i].arcRank = (arcDists.length - 1);
+            }
+            
+            // Add the arc rank to leads to
+            for (var j = 0; j < data.technologies[i].lreq.length; j++) {
+                data.technologies[i].lreq[j].arcRank = data.technologies[i].arcRank;
+            }
+            for (var j = 0; j < data.technologies[i].lopt.length; j++) {
+                data.technologies[i].lopt[j].arcRank = data.technologies[i].arcRank;
             }
         } else {
             data.technologies[i].arcRank = 500;
@@ -94,7 +105,6 @@ d3.json("technologies.json", function(data) {
                         if (data.technologies[i].requires[j] === data.technologies[k].name) {
                             if (data.technologies[k].arcRank < spokeRank) {
                                 spokeRank = data.technologies[k].arcRank;
-                                
                             }
                         }
                     }
@@ -142,7 +152,7 @@ d3.json("technologies.json", function(data) {
             if (!d.requires && !d.optional) {
                 return 0;
             } 
-            return -(180 + (5 * d.spokeRank));
+            return -(arcBase + (arcWidth * d.spokeRank));
         })
         .attr("x2", 0)
         .attr("y2", -(width / 2))
@@ -161,4 +171,46 @@ d3.json("technologies.json", function(data) {
             return color(d.pos);
         })
         .attr("d", arc);
+        
+    var reqSquares = spokes.selectAll(".reqSquare")
+       .data(function(d) {
+           return d.lreq;
+       })
+       .enter().append("g")
+       .attr("transform", function(d) {
+           var ang = d.dist * (360 / data.technologies.length);
+           return "rotate(" + ang + ") translate(0, " + (-arcBase - 2.5 - (arcWidth * d.arcRank)) + ")";
+       })
+       .attr("class", "reqSquare");
+       
+     reqSquares.append("rect")
+        .attr("x", -5)
+        .attr("y", -5)
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", function(d) {
+            return color(d.pos);
+        });
+        
+    var optCircles = spokes.selectAll(".optCircle")
+       .data(function(d) {
+           return d.lopt;
+       })
+       .enter().append("g")
+       .attr("transform", function(d) {
+           console.log(d);
+           var ang = d.dist * (360 / data.technologies.length);
+           return "rotate(" + ang + ") translate(0, " + (-arcBase - 2.5 - (arcWidth * d.arcRank)) + ")";
+       })
+       .attr("class", "optCircle");
+       
+     optCircles.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 4)
+        .attr("stroke-width", 4)
+        .attr("stroke", function(d) {
+            return color(d.pos);
+        })
+        .attr("fill", "white");
 });
