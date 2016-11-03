27d17c197e49a33a2d1b650b9f356eb1080a33c1
Patrick Clarke <ekralccp@me.com>
2016-11-02 21:58:07 -0700

    More for loop refactoring

--- js/dataTools.js
+++ js/dataTools.js
@@ -1,10 +1,10 @@
 // Get a list of the required technology prerequisites for a given thing
-function getReqTechPreReqs(examine, data) {
+var getReqTechPreReqs = function (examine, data) {
     var preReqs = [];
 
     if (examine.requires) {
-        data.technologies.forEach(function(t) {
-            examine.requires.forEach(function(r) {
+        data.technologies.forEach(function (t) {
+            examine.requires.forEach(function (r) {
                 if (r === t.id) {
                     preReqs.push(t);
                 }
@@ -16,117 +16,115 @@ function getReqTechPreReqs(examine, data) {
 }
 
 // Get a list of the displayed things this technology leads to that require it
-function getLeadsToReq(examine, compareData) {
+ var getLeadsToReq = function (examine, compareData) {
     var leads = [];
     
-    for (var i = 0; i < compareData.length; i++) {
-        if (compareData[i].requires) {
-            for (var j = 0; j < compareData[i].requires.length; j++) {
-                if (compareData[i].requires[j] === examine.id) {
-                    leads.push(compareData[i]);
+    compareData.forEach(function (d) {
+        if (d.requires) {
+            d.requires.forEach(function (r) {
+                if(r === examine.id) {
+                    leads.push(d);
                 }
-            }
+            });
         }
-    }
+    });
     
     return leads;
 }
 
 // Get a list of the optional technology prerequisites for a given thing
-function getOptTechPreReqs(examine, data) {
+var getOptTechPreReqs = function (examine, data) {
     var preReqs = [];
 
     if (examine.optional) {
-        for (var i = 0; i < data.technologies.length; i++) {
-            for (var j = 0; j < examine.optional.length; j++) {
-                if (examine.optional[j] === data.technologies[i].id) {
-                    preReqs.push(data.technologies[i]);
+        data.technologies.forEach(function (t) {
+            examine.optional.forEach(function (o) {
+                if (o === t.id) {
+                    preReqs.push(t);
                 }
-            }
-        }
+            });
+        });
     }
     
     return preReqs;
 }
 
 // Get a list of the displayed things this technology leads to that optionally require it
-function getLeadsToOpt(examine, compareData) {
+var getLeadsToOpt = function (examine, compareData) {
     var leads = [];
     
-    for (var i = 0; i < compareData.length; i++) {
-        if (compareData[i].optional) {
-            for (var j = 0; j < compareData[i].optional.length; j++) {
-                if (compareData[i].optional[j] === examine.id) {
-                    leads.push(compareData[i]);
+    compareData.forEach(function (c) {
+        if (c.optional) {
+            c.optional.forEach(function (o) {
+                if (o === examine.id) {
+                    leads.push(c);
                 }
-            }
+            });
         }
-    }
+    });
     
     return leads;
 }
 
 // Get a list of the technology prerequsites (required and optional) for a given thing (techs, units, whatever)
-function getTechPrereqs(examine, data) {
+var getTechPrereqs = function (examine, data) {
     var preReqs = [];
 
     if (examine.requires) {
-        for (var i = 0; i < data.technologies.length; i++) {
-            for (var j = 0; j < examine.requires.length; j++) {
-                if (examine.requires[j] === data.technologies[i].id) {
-                    preReqs.push(data.technologies[i]);
+        data.technologies.forEach(function (t) {
+            examine.requires.forEach(function (r) {
+                if (r === t.id) {
+                    preReqs.push(t);
                 }
-            }
-        }
+            });
+        });
     }
     if (examine.optional) {
-        for (var i = 0; i < data.technologies.length; i++) {
-            for (var j = 0; j < examine.optional.length; j++) {
-                if (examine.optional[j] === data.technologies[i].id) {
-                    preReqs.push(data.technologies[i]);
+        data.technologies.forEach(function (t) {
+            examine.optional.forEach(function (o) {
+                if (o === t.id) {
+                    preReqs.push(t);
                 }
-            }
-        }
+            });
+        });
     }
     
     return preReqs;
 }
 
 // Get a list of the displayed things this technology leads to (required and optional)
-function getLeadsTo(examine, compareData) {
+var getLeadsTo = function (examine, compareData) {
     var leads = [];
     
-    for (var i = 0; i < compareData.length; i++) {
-        if (compareData[i].requires) {
-            for (var j = 0; j < compareData[i].requires.length; j++) {
-                if (compareData[i].requires[j] === examine.id) {
-                    leads.push(compareData[i]);
+    compareData.forEach(function (c) {
+        if (c.requires) {
+            c.requires.forEach(function (r) {
+                if (r === examine.id) {
+                    leads.push(c);
                 }
-            }
+            });
         }
-    }
-    for (var i = 0; i < compareData.length; i++) {
-        if (compareData[i].optional) {
-            for (var j = 0; j < compareData[i].optional.length; j++) {
-                if (compareData[i].optional[j] === examine.id) {
-                    leads.push(compareData[i]);
+        if (c.optional) {
+            c.optional.forEach(function (o) {
+                if (o === examine.id) {
+                    leads.push(c);
                 }
-            }
+            });
         }
-    }
-    
+    });
+
     return leads;
 }
 
 // Returns a technology item by its Id
-function getTechById(examineId, data) {
+var getTechById = function (examineId, data) {
     var tech = "BAD_ID";
 
-    for (var i = 0; i < data.technologies.length; i++) {
-        if (data.technologies[i].id === examineId) {
-            tech = data.technologies[i];
+    data.technologies.forEach(function (t) {
+        if (t.id === examineId) {
+            tech = t;
         }
-    }
+    });
 
     return tech;
 }
@@ -135,8 +133,13 @@ function getTechById(examineId, data) {
 // technologies that it requires (optional & mandatory)
 // technologies it leads to
 // anything else it leads to and the other technologies they require
-function findNearby(origin, data) {
-    var nearbyList = getTechPrereqs(origin, data);
+var findNearby = function (origin, data) {
+    var nearbyList;
+    var fartherList = [];
+    var obsoleteTech;
+    var otherReqs;
+
+    nearbyList = getTechPrereqs(origin, data);
     nearbyList = nearbyList.concat(convertSpecial(origin));
     nearbyList = nearbyList.concat(getLeadsTo(origin, data.technologies));
     nearbyList = nearbyList.concat(getLeadsTo(origin, data.promotions));
@@ -148,21 +151,21 @@ function findNearby(origin, data) {
     nearbyList = nearbyList.concat(getLeadsTo(origin, data.resources));
     nearbyList = nearbyList.concat(getLeadsTo(origin, data.units));
 
-    var fartherList = [];
-    for (var i = 0; i < nearbyList.length; i++) {
-        var otherReqs = getTechPrereqs(nearbyList[i], data);
+    nearbyList.forEach(function (n) {
+        otherReqs = getTechPrereqs(n, data);
 
-        for (var j = 0; j < otherReqs.length; j++) {
-            if (otherReqs[j].id !== origin.id) {
-                fartherList.push(otherReqs[j]);
+        otherReqs.forEach(function (o) {
+            if (o.id !== origin.id) {
+                fartherList.push(o);
             }
-        }
-    }
+        });
+    });
+
     nearbyList = nearbyList.concat(fartherList);
     nearbyList.push(origin);
 
     if (origin.obsolete) {
-        var obsoleteTech = getTechById(origin.obsolete, data);
+        obsoleteTech = getTechById(origin.obsolete, data);
         nearbyList.push(obsoleteTech);
     }
 
@@ -170,17 +173,17 @@ function findNearby(origin, data) {
 }
 
 // Converts the specials in a technology into prereq objects
-function convertSpecial(examine) {
+var convertSpecial = function (examine) {
     var specials = [];
+    var specialItem;
 
     if (examine.special) {
-        for (var i = 0; i < examine.special.length; i++) {
-            var specialItem = examine.special[i];
-            specialItem.requires = [];
-            specialItem.requires.push(examine.id);
-            specialItem.cat = "specials";
-            specials.push(specialItem);
-        }
+        examine.special.forEach(function (s) {
+            s.requires = [];
+            s.requires.push(examine.id);
+            s.cat = "specials";
+            specials.push(s);
+        });
     }
 
     return specials;
