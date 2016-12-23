14bf4dbd1b27b15d0ca6f8a5e3069da52f81234f
Patrick Clarke <ekralccp@me.com>
2016-12-22 21:07:13 -0800

    Hover now hides everything else

--- js/wheelInteraction.js
+++ js/wheelInteraction.js
@@ -15,76 +15,168 @@ var selLine = function(setId, setClass, toShow) {
         .classed(setClass, toShow);
 };
 
-function spokeHighlightIn(d) {
-    selLine(d.id, "lineSelected", true);
+var fader = function(setId, toShow) {
+    d3.selectAll("." + setId)
+        .classed("fade", toShow);
+};
+
+var makeTempArc = function(highlighted, setId, data) {
+    var reqD = getTechById(setId, data);
+    var tempDist; // TODO: Need to include leads to!
+    var tempBack;
+    if (highlighted.pos > reqD.pos) {
+        tempDist = highlighted.pos - reqD.pos;
+        tempBack = 0;
+    } else {
+        tempDist = 0;
+        tempBack = reqD.pos - highlighted.pos;
+    }
+    reqD.tempArcDist = ((2 * Math.PI) / data.displayed.length) * tempDist;
+    reqD.tempArcBack = ((2 * Math.PI) / data.displayed.length) * tempBack;
+
+    var tempArcG = d3.select(".spokeAll") // TODO: move to reqArcs
+        .select("." + reqD.id)
+        .append("g")
+        .attr("class", "tempArcG");
+
+    tempArcG.append("path")
+        .attr("class", "tempArc")
+        .style("fill", function(d) {
+            return CIV.color(d.pos);
+        })
+        .attr("d", tempArc);
+
+    tempArcG.append("line")
+        .attr("class", "tempSpokePin")
+        .attr("x1", 0)
+        .attr("y1", function(d) {
+            return -(CIV.arcBase + 7 + (CIV.arcSpace * reqD.arcRank));
+        })
+        .attr("x2", 0)
+        .attr("y2", function(d) {
+            return -(CIV.arcBase - 5 + (CIV.arcSpace * reqD.arcRank));
+        })
+        .attr("stroke-width", CIV.arcWidth)
+        .attr("stroke", function(d) {
+            return CIV.color(reqD.pos); 
+        });
+
+    var tempReqSquares = tempArcG.selectAll(".tempReqSquare")
+        .data(function(d) {
+            return d.lreq;
+        })
+        .enter().append("g")
+        .filter(function(d) {
+            return d.id === highlighted.id;
+        })
+        .attr("transform", function(d) {
+            var ang = d.dist * (360 / data.displayed.length);
+            return "rotate(" + ang + ") translate(0, " + (-CIV.arcBase - 2.5 - (CIV.arcSpace * d.arcRank)) + ")";
+        })
+        .attr("class", "reqSquare");
+
+    tempReqSquares.append("rect")
+        .attr("x", -2.5)
+        .attr("y", -0.75)
+        .attr("width", 5)
+        .attr("height", 5)
+        .attr("fill", function(d) {
+            return CIV.color(d.pos);
+        });
+
+    var tempOptCircles = tempArcG.selectAll(".tempOptCircle")
+        .data(function(d) {
+            return d.lopt;
+        })
+        .enter().append("g")
+        .filter(function(d) {
+            return d.id === highlighted.id;
+        })
+        .attr("transform", function(d) {
+            var ang = d.dist * (360 / data.displayed.length);
+            return "rotate(" + ang + ") translate(0, " + (-CIV.arcBase - 2.5 - (CIV.arcSpace * d.arcRank)) + ")";
+        })
+        .attr("class", "optCircle");
+    
+    tempOptCircles.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 2)
+        .attr("r", 2.5)
+        .attr("stroke-width", 1)
+        .attr("stroke", function(d) {
+            return CIV.color(d.pos);
+        })
+        .attr("fill", "white");
+}
+
+function spokeHighlightIn(d, data) {
+    d3.selectAll(".spoke")
+        .classed("fade", true);
+    d3.select(".reqArcs")
+        .selectAll(".reqGroup")
+        .classed("fade", true);
+
+    d3.select("." + d.id)
+        .classed("fade", false);
+    d3.select(".reqArcs")
+        .select("." + d.id)
+        .classed("fade", false);
 
     if (d.requires) {
         if (Array.isArray(d.requires)) {
             d.requires.forEach(function (r) {
-                selLine(r, "lineRequired", true);
+                d3.select(".spokeAll")
+                    .selectAll("." + r)
+                    .classed("fade", false);
+
+                makeTempArc(d, r, data);
             });
         } else {
-            selLine(d.requires, "lineRequired", true);
+            d3.select(".spokeAll")
+                .selectAll("." + d.requires)
+                .classed("fade", false);
+            makeTempArc(d, d.requires, data);
         }
     }
     if (d.optional) {
         if (Array.isArray(d.optional)) {
             d.optional.forEach(function (o) {
-                selLine(o, "lineOptional", true);
+                d3.select(".spokeAll")
+                    .selectAll("." + o)
+                    .classed("fade", false);
+
+                makeTempArc(d, o, data);
             });
         } else {
-            selLine(d.optional, "lineOptional", true);
+            d3.select(".spokeAll")
+                .selectAll("." + d.optional)
+                .classed("fade", false);
+            makeTempArc(d, d.optional, data);
         }
     }
     if (d.lreq) {
         d.lreq.forEach(function (lr) {
-            d3.selectAll("." + lr.id)
-            .selectAll(".spokeLine")
-                .classed("lineLeads", true);
+            d3.select(".spokeAll")
+                .selectAll("." + lr.id)
+                .classed("fade", false);
         });
     }
     if (d.lopt) {
         d.lopt.forEach(function (lo) {
-            d3.selectAll("." + lo.id)
-            .selectAll(".spokeLine")
-                .classed("lineLeads", true);
+            d3.select(".spokeAll")
+                .selectAll("." + lo.id)
+                .classed("fade", false);
         });
     }
+
+
 }
 
 function spokeHighlightOut(d) {
-    selLine(d.id, "lineSelected", false);
-
-    if (d.requires) {
-        if (Array.isArray(d.requires)) {
-            d.requires.forEach(function (r) {
-                selLine(r, "lineRequired", false);
-            });
-        } else {
-            selLine(d.requires, "lineRequired", false);
-        }
-    }
-    if (d.optional) {
-        if (Array.isArray(d.optional)) {
-            d.optional.forEach(function (o) {
-                selLine(o, "lineOptional", false);
-            });
-        } else {
-            selLine(d.optional, "lineOptional", false);
-        }
-    }
-    if (d.lreq) {
-        d.lreq.forEach(function (lr) {
-            d3.selectAll("." + lr.id)
-            .selectAll(".spokeLine")
-                .classed("lineLeads", false);
-        });
-    }
-    if (d.lopt) {
-        d.lopt.forEach(function (lo) {
-            d3.selectAll("." + lo.id)
-            .selectAll(".spokeLine")
-                .classed("lineLeads", false);
-        });
-    }
+    d3.selectAll(".spoke")
+        .classed("fade", false);
+    d3.selectAll(".reqGroup")
+        .classed("fade", false);
+    d3.selectAll(".tempArcG")
+        .remove();
 }
