8c92031ca27248f103b877d6d957f8d1ff8a98ca
Patrick Clarke <ptrckcclrk@gmail.com>
2019-10-23 21:36:42 -0700

    Added reactified build to pages, removed irrelevant code

---
+++ precache-manifest.6ff269d077d1b5561541f5c49afd7572.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e57adfe58508104af84e6305ebf8208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8ace95ce4d26d026197",
+    "url": "/static/css/main.24d1a911.chunk.css"
+  },
+  {
+    "revision": "cf12a9c67977580b71c7",
+    "url": "/static/js/2.8be16f39.chunk.js"
+  },
+  {
+    "revision": "b8ace95ce4d26d026197",
+    "url": "/static/js/main.94830f9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a2e138bf043baf91cc58977789c1767",
+    "url": "/static/media/goudytrajan-regular-trial.1a2e138b.ttf"
+  },
+  {
+    "revision": "261792acdeae51f8177c95deebf48465",
+    "url": "/static/media/goudytrajan-regular-trial.261792ac.woff"
+  },
+  {
+    "revision": "2e5b2d447cb03c3dd6f0b8cebd834d7c",
+    "url": "/static/media/goudytrajan-regular-trial.2e5b2d44.svg"
+  },
+  {
+    "revision": "767ce0729e11c20e7fdf90b3081c986b",
+    "url": "/static/media/goudytrajan-regular-trial.767ce072.eot"
+  },
+  {
+    "revision": "846283374ce79fb79222db70dab066ec",
+    "url": "/static/media/goudytrajan-regular-trial.84628337.woff2"
+  },
+  {
+    "revision": "ab4a049922fd9f6d39cfe07ad702a8f2",
+    "url": "/static/media/startSlice.ab4a0499.png"
+  }
+]);
