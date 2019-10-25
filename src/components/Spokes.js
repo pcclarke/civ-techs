7d30b3120fc3633e1880e00480eb210aa911fd76
Patrick Clarke <ptrckcclrk@gmail.com>
2019-10-24 21:19:44 -0700

    Extracted spokes into separate component

---
+++ src/components/Spokes.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+
+import {setImageLink} from '../libs/stringTools.js';
+
+import Arc from './Arc';
+import Relationship from './Relationship';
+
+function Spokes(props) {
+  const {
+    angleShift,
+    arcBaseRadius,
+    arcSpace,
+    arcStrokeWidth,
+    colour,
+    data,
+    displayUnlockModal,
+    game,
+    empire,
+    notFaded,
+    notUnlockFaded,
+    setFade,
+    setNotFaded,
+    setUnlockFade,
+    updateDataFade,
+    updateUnlockFade,
+    width
+  } = props;
+
+  return (
+    <g className='spokes'>
+      {
+        data.displayed.map((d, i) => (
+          <g
+            className={`${d.id} spoke`}
+            key={`spoke-${i}`}
+            transform={`rotate(${d.pos * (360 / data.displayed.length) + angleShift})`}
+          >
+            <line
+              className={`spokeLine ${(notFaded.length > 0) ? 'fade' : ''}`}
+              x1={0}
+              y1={(!d.requires && !d.optional) ? 0 : -(arcBaseRadius + (arcSpace * d.spokeRank))}
+              x2={0}
+              y2={-(width / 2) + 160 - (d.unlocks.length * 14)}
+            />
+            <image
+              className={`techImg ${setFade(d)}`}
+              height={25}
+              onClick={() => displayUnlockModal(d, data)}
+              onMouseLeave={() => setNotFaded([])}
+              onMouseOver={() => updateDataFade(d)}
+              transform={(() => (d.pos > (data.displayed.length / 2)) ?
+                `translate(10, ${(-(width / 2) + 157)}) rotate(90)` :
+                `translate(-10, ${(-(width / 2) + 182)}) rotate(270)`)()}
+              width={25}
+              xlinkHref={`${game}/${d.cat}/${d.id}.png`}
+            />
+            {d.unlocks.map((u, j) => (
+              <g key={`unlock-${j}`}>
+                {u.lreq &&
+                  <g className={`unlock ${(notUnlockFaded === u.ref.id) ? '' : 'opaque'} ${u.ref.id}${u.pos}`}>
+                    <Arc
+                      baseRadius={442.5}
+                      className='unlockArc'
+                      colour={colour}
+                      data={u}
+                      space={14}
+                      strokeWidth={arcStrokeWidth}
+                    />
+                    {u.lreq.map((l, k) => (
+                      <Relationship
+                        baseRadius={(width/2) - 147.5}
+                        colour={colour}
+                        data={l}
+                        key={`unlock-square-${k}`}
+                        shape={'square'}
+                        space={14}
+                        totalTechnologies={data.displayed.length}
+                      />
+                    ))}
+                  </g>
+                }
+                <image
+                  className={`unlockIcon ${setFade(d)} ${setUnlockFade(u.ref.id)}`}
+                  height={13}
+                  onClick={() => displayUnlockModal(u.ref, data)}
+                  onMouseLeave={() => updateUnlockFade()}
+                  onMouseOver={() => updateUnlockFade(u.ref.id)}
+                  transform={(() => (u.pos > (data.displayed.length / 2)) ?
+                    `translate(6, ${(-(width / 2) + (142 - (14 * u.rank)))}) rotate(90)` :
+                    `translate(-6, ${(-(width / 2) + (153 - (14 * u.rank)))}) rotate(270)`)()}
+                  width={13}
+                  xlinkHref={(() => setImageLink(u.ref, game, empire))()}
+                />
+              </g>
+            ))}
+          </g>
+        ))
+      }
+    </g>
+  );
+}
+
+export default Spokes;
--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -11,10 +11,9 @@ import {setImageLink, oxfordizer} from '../libs/stringTools.js';
 import {scaleOrdinal as d3_scaleOrdinal} from 'd3-scale';
 import {schemeCategory10 as d3_schemeCategory10} from 'd3-scale-chromatic';
 
-import Arc from './Arc';
 import LeadsTo from './LeadsTo';
-import Relationship from './Relationship';
 import RequirementsModal from './RequirementsModal';
+import Spokes from './Spokes';
 
 import startSlice from '../img/startSlice.png';
 
@@ -275,76 +274,25 @@ function Wheel(props) {
             </g>
           }
 
-          <g className='spokeAll'>
-            {
-              data.displayed.map((d, i) => (
-                <g
-                  className={`${d.id} spoke`}
-                  key={`spoke-${i}`}
-                  transform={`rotate(${d.pos * (360 / data.displayed.length) + angleShift})`}
-                >
-                  <line
-                    className={`spokeLine ${(notFaded.length > 0) ? 'fade' : ''}`}
-                    x1={0}
-                    y1={(!d.requires && !d.optional) ? 0 : -(arcBaseRadius + (arcSpace * d.spokeRank))}
-                    x2={0}
-                    y2={-(width / 2) + 160 - (d.unlocks.length * 14)}
-                  />
-                  <image
-                    className={`techImg ${setFade(d)}`}
-                    height={25}
-                    onClick={() => displayUnlockModal(d, data)}
-                    onMouseLeave={() => setNotFaded([])}
-                    onMouseOver={() => updateDataFade(d)}
-                    transform={(() => (d.pos > (data.displayed.length / 2)) ?
-                      `translate(10, ${(-(width / 2) + 157)}) rotate(90)` :
-                      `translate(-10, ${(-(width / 2) + 182)}) rotate(270)`)()}
-                    width={25}
-                    xlinkHref={`${game}/${d.cat}/${d.id}.png`}
-                  />
-                  {d.unlocks.map((u, j) => (
-                    <g key={`unlock-${j}`}>
-                      {u.lreq &&
-                        <g className={`unlock ${(notUnlockFaded === u.ref.id) ? '' : 'opaque'} ${u.ref.id}${u.pos}`}>
-                          <Arc
-                            baseRadius={442.5}
-                            className='unlockArc'
-                            colour={color}
-                            data={u}
-                            space={14}
-                            strokeWidth={arcStrokeWidth}
-                          />
-                          {u.lreq.map((l, k) => (
-                            <Relationship
-                              baseRadius={(width/2) - 147.5}
-                              colour={color}
-                              data={l}
-                              key={`unlock-square-${k}`}
-                              shape={'square'}
-                              space={14}
-                              totalTechnologies={data.displayed.length}
-                            />
-                          ))}
-                        </g>
-                      }
-                      <image
-                        className={`unlockIcon ${setFade(d)} ${setUnlockFade(u.ref.id)}`}
-                        height={13}
-                        onClick={() => displayUnlockModal(u.ref, data)}
-                        onMouseLeave={() => updateUnlockFade()}
-                        onMouseOver={() => updateUnlockFade(u.ref.id)}
-                        transform={(() => (u.pos > (data.displayed.length / 2)) ?
-                          `translate(6, ${(-(width / 2) + (142 - (14 * u.rank)))}) rotate(90)` :
-                          `translate(-6, ${(-(width / 2) + (153 - (14 * u.rank)))}) rotate(270)`)()}
-                        width={13}
-                        xlinkHref={(() => setImageLink(u.ref, game, empire))()}
-                      />
-                    </g>
-                  ))}
-                </g>
-              ))
-            }
-          </g>
+          <Spokes
+            angleShift={angleShift}
+            arcBaseRadius={arcBaseRadius}
+            arcSpace={arcSpace}
+            arcStrokeWidth={arcStrokeWidth}
+            colour={color}
+            data={data}
+            displayUnlockModal={displayUnlockModal}
+            game={game}
+            empire={empire}
+            notFaded={notFaded}
+            notUnlockFaded={notUnlockFaded}
+            setFade={setFade}
+            setNotFaded={setNotFaded}
+            setUnlockFade={setUnlockFade}
+            updateDataFade={updateDataFade}
+            updateUnlockFade={updateUnlockFade}
+            width={width}
+          />
 
           <g className='reqArcs'>
             {data.displayed.map((d, i) => (
